ab09aa020360eb6f63bd8beb599c2344f107352b
sonGit-Kraft <sdhunituser@gmail.com>
2025-04-09 15:03:57 +0900

    Study

---
+++ Week_06/GaemaMusa/Assets/Scripts/Entity.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    [Header("Collider Info")]
+    [SerializeField] protected Transform groundCheck;
+    [SerializeField] protected float groundCheckDistance;
+    [SerializeField] protected Transform wallCheck;
+    [SerializeField] protected float wallCheckDistance;
+    [SerializeField] protected LayerMask whatIsGround;
+
+    #region Components
+    public Animator anim { get; private set; }
+    public Rigidbody2D rb { get; private set; }
+    #endregion
+
+    public int facingDir { get; private set; } = 1;
+    protected bool facingRight = true;
+
+    protected virtual void Awake()
+    {
+
+    }
+
+    protected virtual void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    protected virtual void Update()
+    {
+
+    }
+
+    #region Collision
+    public virtual bool IsGroundDetected() => Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+    public virtual bool IsWallDetected() => Physics2D.Raycast(wallCheck.position, Vector2.right * facingDir, wallCheckDistance, whatIsGround);
+
+    protected virtual void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
+    }
+    #endregion
+
+    #region Flip
+    public virtual void Flip()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    public virtual void FlipController(float _x)
+    {
+        if (_x > 0 && !facingRight) // 오른쪽 이동인데 왼쪽을 바라보고 있을 때
+            Flip();
+        else if (_x < 0 && facingRight) // 왼쪽 이동인데 오른쪽을 바라보고 있을 때
+            Flip();
+    }
+    #endregion
+
+    #region Velocity 
+    public void ZeroVelocity() => rb.linearVelocity = new Vector2(0, 0);
+
+    public void SetVelocity(float _xVelocity, float _yVelocity)
+    {
+        rb.linearVelocity = new Vector2(_xVelocity, _yVelocity);
+        FlipController(_xVelocity);
+    }
+    #endregion
+}
--- Week_06/GaemaMusa/Assets/Scripts/Player/Player.cs
+++ Week_06/GaemaMusa/Assets/Scripts/Player/Player.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Entity
 {
     [Header("Attack Detail")]
     public Vector2[] attackMovement;
@@ -17,21 +17,7 @@ public class Player : MonoBehaviour
     public float dashDuration;
     public float dashDir { get; private set; }
 
-    [Header("Collider Info")]
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private float groundCheckDistance;
-    [SerializeField] private Transform wallCheck;
-    [SerializeField] private float wallCheckDistance;
-    [SerializeField] private LayerMask whatIsGround;
-
     public bool isBusy { get; private set; }
-    public int facingDir { get; private set; } = 1;
-    private bool facingRight = true;
-
-    #region Components
-    public Animator anim { get; private set; }
-    public Rigidbody2D rb { get; private set; }
-    #endregion
 
     #region States
     public PlayerStateMachine stateMachine { get; private set; }
@@ -45,8 +31,10 @@ public class Player : MonoBehaviour
     public PlayerPrimaryAttack attackState { get; private set; }
     #endregion
 
-    private void Awake()
+    protected override void Awake()
     {
+        base.Awake();
+
         stateMachine = new PlayerStateMachine();
         idleState = new PlayerIdleState(this, stateMachine, "Idle");
         moveState = new PlayerMoveState(this, stateMachine, "Move");
@@ -58,16 +46,15 @@ public class Player : MonoBehaviour
         attackState = new PlayerPrimaryAttack(this, stateMachine, "Attack");
     }
 
-    private void Start()
+    protected override void Start()
     {
-        anim = GetComponentInChildren<Animator>();
-        rb = GetComponent<Rigidbody2D>();
-
+        base.Start();
         stateMachine.Initialize(idleState);
     }
 
-    private void Update()
+    protected override void Update()
     {
+        base.Update();
         stateMachine.currentState.Update();
         CheckForDashInput();
     }
@@ -100,42 +87,4 @@ public class Player : MonoBehaviour
             stateMachine.ChangeState(dashState);
         }
     }
-
-    #region Velocity 
-    public void ZeroVelocity() => rb.linearVelocity = new Vector2(0, 0);
-
-    public void SetVelocity(float _xVelocity, float _yVelocity)
-    {
-        rb.linearVelocity = new Vector2(_xVelocity, _yVelocity);
-        FlipController(_xVelocity);
-    }
-    #endregion
-
-    #region Collision
-    public bool IsGroundDetected() => Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
-    public bool IsWallDetected() => Physics2D.Raycast(wallCheck.position, Vector2.right * facingDir, wallCheckDistance, whatIsGround);
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
-        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
-    }
-    #endregion
-
-    #region Flip
-    public void Flip()
-    {
-        facingDir = facingDir * -1;
-        facingRight = !facingRight;
-        transform.Rotate(0, 180, 0);
-    }
-
-    public void FlipController(float _x)
-    {
-        if(_x > 0 && !facingRight) // 오른쪽 이동인데 왼쪽을 바라보고 있을 때
-            Flip();
-        else if(_x < 0 && facingRight) // 왼쪽 이동인데 오른쪽을 바라보고 있을 때
-            Flip();
-    }
-    #endregion
 }
